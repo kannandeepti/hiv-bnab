94d7df908c625150ce5375dbbd55165f9e4b1449
Deepti Kannan <kannandeepti@yahoo.com>
2024-07-23 12:00:41 -0400

    Added .gitignore file, cleaned up unused folders

--- coevolution/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -n 2
-#SBATCH -N 1
-#SBATCH -o logs/%j.out
-
-
-source activate biopython
-
-python -u run.py $1 $2 >& logs/${1}_${2}.out
--- working_code/__init__.py
+++
@@ -1,8 +0,0 @@
-""" 
-Simulations of Affinity Maturation 
-==================================
-TODO: insert high level description of code
-
-"""
-
-__version__ = "0.1.0"
